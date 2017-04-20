fd2ffe2cf5981aace60faada48ec8b7f9cfe1dd3
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-04-20 18:58:25 +0200

    Création template Todos.vue + css todos.css

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+  <todos></todos>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+    /* eslint-disable indent */
+
+    import Todos from './components/Todos'
+
+  export default {
+
+    components: {
+        Todos
+    }
+
+  }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <sections class="todoapp">
+        <header class="header">
+            <h1>Todos</h1>
+        </header>
+    </sections>
+
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style></style>
